03579fd70c43c654d783bd83a021ebabfd17e429
ashe-sparcs <wotjd1102@gmail.com>
2016-12-11 16:23:14 +0900

    original project and file chooser successfully merged. now working on file input and output.

--- app/src/main/java/com/example/madstein/a20140679_proj5/MainActivity.java
+++ app/src/main/java/com/example/madstein/a20140679_proj5/MainActivity.java
@@ -1,8 +1,27 @@
+
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.madstein.a20140679_proj5;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,6 +36,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetSocketAddress;
@@ -27,24 +47,71 @@ import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.provider.DocumentsContract.Document;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
-    TextView textResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Example that exercises client side of {@link DocumentsContract}.
+ */
+public class MainActivity extends Activity {
+
+    //Variables for original application
+    TextView textResponse, textFind;
     EditText editTextAddress, editTextPort;
-    Button buttonConnect;
+    Button buttonConnect, buttonFindInput;
     RadioGroup radioGroupOperation;
     RadioButton radioButtonEncrypt, radioButtonDecrypt;
     Spinner spinnerShift;
 
+    // Variables for file chooser
+    private static final String TAG = "DocumentsSample";
+    private static final int CODE_READ = 42;
+    /*
+    private static final int CODE_WRITE = 43;
+    private static final int CODE_TREE = 44;
+    private static final int CODE_RENAME = 45;
+    */
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         setContentView(R.layout.activity_main);
+        buttonFindInput = (Button)findViewById(R.id.find);
+        buttonFindInput.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("*/*");
+                startActivityForResult(intent, CODE_READ);
+            }
+        });
 
         editTextAddress = (EditText)findViewById(R.id.address);
         editTextPort = (EditText)findViewById(R.id.port);
         buttonConnect = (Button)findViewById(R.id.connect);
         textResponse = (TextView)findViewById(R.id.response);
+        textFind = (TextView)findViewById(R.id.find_result);
         radioGroupOperation = (RadioGroup) findViewById(R.id.operation);
         radioButtonEncrypt = (RadioButton) findViewById(R.id.encrypt);
         radioButtonDecrypt = (RadioButton) findViewById(R.id.decrypt);
@@ -54,60 +121,60 @@ public class MainActivity extends AppCompatActivity {
     }
 
     View.OnClickListener buttonConnectOnClickListener =
-        new View.OnClickListener(){
-            @Override
-            public void onClick(View arg0) {
-                byte operationID = 2;
-                byte shiftValue;
-                if (radioButtonEncrypt.isChecked()) {
-                    operationID = 0;
-                } else if(radioButtonDecrypt.isChecked()) {
-                    operationID = 1;
+            new View.OnClickListener(){
+                @Override
+                public void onClick(View arg0) {
+                    byte operationID = 2;
+                    byte shiftValue;
+                    if (radioButtonEncrypt.isChecked()) {
+                        operationID = 0;
+                    } else if(radioButtonDecrypt.isChecked()) {
+                        operationID = 1;
+                    }
+                    String addressString = editTextAddress.getText().toString();
+                    String portString = editTextPort.getText().toString();
+                    String shiftString = spinnerShift.getSelectedItem().toString();
+                    if (addressString.matches("") || portString.matches("")) {
+                        AlertDialog.Builder dstAlert = new AlertDialog.Builder(MainActivity.this);
+                        dstAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                        dstAlert.setMessage("Enter address and port");
+                        dstAlert.show();
+                    } else if (operationID == 2) {
+                        AlertDialog.Builder operationAlert = new AlertDialog.Builder(MainActivity.this);
+                        operationAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                        operationAlert.setMessage("Encryption/Decryption check required");
+                        operationAlert.show();
+                    } else if (shiftString.matches("Shift")) {
+                        AlertDialog.Builder shiftAlert = new AlertDialog.Builder(MainActivity.this);
+                        shiftAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                        shiftAlert.setMessage("Select shift value");
+                        shiftAlert.show();
+                    } else {
+                        shiftValue = (byte)Integer.parseInt(shiftString);
+                        MyClientTask myClientTask = new MyClientTask(
+                                addressString,
+                                Integer.parseInt(portString),
+                                operationID,
+                                shiftValue);
+                        myClientTask.execute();
+                    }
                 }
-                String addressString = editTextAddress.getText().toString();
-                String portString = editTextPort.getText().toString();
-                String shiftString = spinnerShift.getSelectedItem().toString();
-                if (addressString.matches("") || portString.matches("")) {
-                    AlertDialog.Builder dstAlert = new AlertDialog.Builder(MainActivity.this);
-                    dstAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    });
-                    dstAlert.setMessage("Enter address and port");
-                    dstAlert.show();
-                } else if (operationID == 2) {
-                    AlertDialog.Builder operationAlert = new AlertDialog.Builder(MainActivity.this);
-                    operationAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    });
-                    operationAlert.setMessage("Encryption/Decryption check required");
-                    operationAlert.show();
-                } else if (shiftString.matches("Shift")) {
-                    AlertDialog.Builder shiftAlert = new AlertDialog.Builder(MainActivity.this);
-                    shiftAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    });
-                    shiftAlert.setMessage("Select shift value");
-                    shiftAlert.show();
-                } else {
-                    shiftValue = (byte)Integer.parseInt(shiftString);
-                    MyClientTask myClientTask = new MyClientTask(
-                            addressString,
-                            Integer.parseInt(portString),
-                            operationID,
-                            shiftValue);
-                    myClientTask.execute();
-                }
-            }
-        };
+            };
 
     public int realLength(byte[] data) {
         int size = 0;
@@ -120,12 +187,80 @@ public class MainActivity extends AppCompatActivity {
         return size;
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        final ContentResolver cr = getContentResolver();
+
+        clearLog();
+
+        log("resultCode=" + resultCode);
+        log("data=" + String.valueOf(data));
+
+        final Uri uri = data != null ? data.getData() : null;
+        if (uri != null) {
+            log("isDocumentUri=" + DocumentsContract.isDocumentUri(this, uri));
+        } else {
+            log("missing URI?");
+            return;
+        }
+
+        if (requestCode == CODE_READ) {
+            try {
+                cr.takePersistableUriPermission(uri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            } catch (SecurityException e) {
+                log("FAILED TO TAKE PERMISSION", e);
+            }
+            InputStream is = null;
+            try {
+                is = cr.openInputStream(uri);
+                log("read length=" + readFullyNoClose(is).length);
+            } catch (Exception e) {
+                log("FAILED TO READ", e);
+            } finally {
+                closeQuietly(is);
+            }
+        }
+    }
+
+    private void clearLog() {
+        textFind.setText(null);
+    }
+
+    private void log(String msg) {
+        log(msg, null);
+    }
+
+    private void log(String msg, Throwable t) {
+        Log.d(TAG, msg, t);
+        textFind.setText(textFind.getText() + "\n" + msg);
+    }
+
+    public static byte[] readFullyNoClose(InputStream in) throws IOException {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int count;
+        while ((count = in.read(buffer)) != -1) {
+            bytes.write(buffer, 0, count);
+        }
+        return bytes.toByteArray();
+    }
+
+    public static void closeQuietly(AutoCloseable closeable) {
+        if (closeable != null) {
+            try {
+                closeable.close();
+            } catch (RuntimeException rethrown) {
+                throw rethrown;
+            } catch (Exception ignored) {
+            }
+        }
+    }
+
     public class MyClientTask extends AsyncTask<Void, Void, Void> {
 
         String dstAddress;
         int dstPort;
         byte operationID, shift;
-        //String request = "aaaaaaaaaaaaaaaaaaaa";
         String request = "abcdeabcdeabcde";
         String requestOriginal = "abcdeabcdeabcde";
         String response = "";
